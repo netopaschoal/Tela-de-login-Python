59960295383645dab4887c35a1d7c45ad00807df
José Luiz Paschoal Neto <62352083+netopaschoal@users.noreply.github.com>
2020-09-01 17:10:36 -0300

    Add files via upload

--- tela_login/login.py
+++ tela_login/login.py
@@ -1,4 +1,5 @@
 from PyQt5 import  uic,QtWidgets
+import sqlite3
 
 
 def chama_segunda_tela():
@@ -16,12 +17,46 @@ def logout():
     segunda_tela.close()
     primeira_tela.show()
 
+def abre_tela_cadastro():
+    tela_cadastro.show()
+
+
+def cadastrar():
+    nome = tela_cadastro.lineEdit.text()
+    login = tela_cadastro.lineEdit_2.text()
+    senha = tela_cadastro.lineEdit_3.text()
+    c_senha = tela_cadastro.lineEdit_4.text()
+
+    if (senha == c_senha):
+        try:
+            banco = sqlite3.connect('banco_cadastro.db') 
+            cursor = banco.cursor()
+            cursor.execute("CREATE TABLE IF NOT EXISTS cadastro (nome text,login text,senha text)")
+            cursor.execute("INSERT INTO cadastro VALUES ('"+nome+"','"+login+"','"+senha+"')")
+
+            banco.commit() 
+            banco.close()
+            tela_cadastro.label.setText("Usuario cadastrado com sucesso")
+
+        except sqlite3.Error as erro:
+            print("Erro ao inserir os dados: ",erro)
+    else:
+        tela_cadastro.label.setText("As senhas digitadas estão diferentes")
+    
+
+    
+
+
 app=QtWidgets.QApplication([])
 primeira_tela=uic.loadUi("primeira_tela.ui")
 segunda_tela = uic.loadUi("segunda_tela.ui")
+tela_cadastro = uic.loadUi("tela_cadastro.ui")
 primeira_tela.pushButton.clicked.connect(chama_segunda_tela)
 segunda_tela.pushButton.clicked.connect(logout)
 primeira_tela.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+primeira_tela.pushButton_2.clicked.connect(abre_tela_cadastro)
+tela_cadastro.pushButton.clicked.connect(cadastrar) 
+
 
 primeira_tela.show()
 app.exec()
